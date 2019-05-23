c5b6b552780aae5e71252e74f551a59779b68563
KumaranM <kumaranm2040@gmail.com>
2019-05-23 21:19:46 +0200

    Added routing functionality and filtering of routes.

--- Javascript/Node/01-understanding-npm-scripts/app.js
+++ Javascript/Node/01-understanding-npm-scripts/app.js
@@ -1,16 +1,21 @@
 const express = require('express');
-//const routes = require('./routes');
+
+const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
-app.use('/add-product',(req, res, next)=> {
-    console.log('In the middleware');
-    res.send('<h1>Hello from Express Add Product!</h1>');
-});
 
-app.use('/', (req, res, next)=> {
-    console.log('In the other middleware');
-    res.send('<h1>Hello from Express!</h1>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+
+app.use(shopRoutes);
+
+app.use((req, res,next)=> {
+  res.status(404).send('<h1>Page Not Found</h1>');
+})
 
 
 app.listen(3000);
