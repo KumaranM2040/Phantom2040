41b31ca1908a8c1e0eaa6479e26f0ad20781914e
KumaranM <kumaranm2040@gmail.com>
2019-05-25 22:44:01 +0200

    Brought it Bootstrap 4.3 Framework for UI and slowly swapping example to use bootstrap features. Site looks alot better.

--- Javascript/Node/01-understanding-npm-scripts/app.js
+++ Javascript/Node/01-understanding-npm-scripts/app.js
@@ -13,9 +13,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/admin', adminRoutes);
 
 app.use(shopRoutes);
+app.use(express.static('public'));
 
 app.use((req, res,next)=> {
-  res.status(404).sendFile(path.join(__dirname, 'views', 'error-404.html'));
+  res.status(404).sendFile(path.join(__dirname, 'views','error-404.html'));
 })
 
 app.listen(3000);
--- Javascript/Node/01-understanding-npm-scripts/routes/shop.js
+++ Javascript/Node/01-understanding-npm-scripts/routes/shop.js
@@ -3,10 +3,11 @@ const path = require('path');
 const express = require('express');
 
 const router = express.Router();
+const rootDir = require('../util/path');
 
 router.get('/', (req, res, next)=> {
     //console.log('In the other middleware');
-    res.sendFile(path.join(__dirname, '../','views', 'shop.html'));
+    res.sendFile(path.join(rootDir,'index.html'));
 });
 
 module.exports = router;
