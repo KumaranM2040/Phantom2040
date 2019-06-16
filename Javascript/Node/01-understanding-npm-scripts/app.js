0b8e388028dcf04237f5c11532e9ed3bbefdebc7
KumaranM <kumaranm2040@gmail.com>
2019-06-16 18:18:53 +0200

    Added error-404.pug file and serve it up. Added custom.css file to centre align "Bust Jim" and page not found text.

--- Javascript/Node/01-understanding-npm-scripts/app.js
+++ Javascript/Node/01-understanding-npm-scripts/app.js
@@ -17,7 +17,7 @@ app.use(shopRoutes);
 app.use(express.static('public'));
 
 app.use((req, res,next)=> {
-  res.status(404).sendFile(path.join(__dirname, 'views','error-404.html'));
+  res.status(404).render('error-404');
 })
 
 app.listen(3000);
