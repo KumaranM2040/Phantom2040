d536f4ed10a68c1829bb44556cf3e0a997439c0f
Kumaran Moodley <kumaranm@sahomeloans.com>
2019-06-28 21:52:21 +0200

    Added partial parsing of SQL Extract

--- Javascript/Node/simple-website/app.js
+++ Javascript/Node/simple-website/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const fs = require('fs');
 
 var app = express();
 const expressNunjucks = require('express-nunjucks');
@@ -21,7 +22,9 @@ const njk = expressNunjucks(app, {
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -29,12 +32,80 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
+function TokeniseString(line) {
+  return line.split('|');
+}
+
+function ParseGlobalExtractHeaders(globalHeaders) {
+  let model = {};
+  let tokens = TokeniseString(globalHeaders);
+  model.columns = [];
+  console.log(tokens[0]);
+  tokens.forEach(token => {
+    if (isNaN(token)) {
+      model.columns.push(token);
+    }
+  });
+  return model.columns;
+}
+
+function ParseTableHeaders(tokens) {
+  let model = {};
+  model.columns = [];
+  console.log(tokens[0]);
+  tokens.forEach(token => {
+    if (isNaN(token)) {
+      model.columns.push(token);
+    }
+  });
+}
+
+function ParseExtract() {
+  const buffer = Buffer.allocUnsafe(5 * 1024 * 1024);
+  const statementObject = {
+    accounts: [],
+    TableHeaders: []
+  };
+  const size = 0;
+  var file = fs.createReadStream('C:\\Work\\EmailingLoanStatementsToClient\\SAHL_LoanStatements_20190523\\SAHL_LoanStatements_20190523.txt');
+  file.on('data', (chunk) => {
+    console.log(`Received ${chunk.length} bytes of data.`);
+    if (chunk.length) {
+      console.log(chunk.length);
+      //console.log(chunk.toString(););
+      let chunkstr = chunk.toString();
+
+      let tokens = chunkstr.split('|');
+      let newlines = chunkstr.split('\n');
+      newlines.forEach(line => {
+        if (line.startsWith('Section')) {
+          let globalHeader = ParseGlobalExtractHeaders(newlines[0]);
+          console.log(globalHeader);
+          statementObject.TableHeaders = globalHeader;
+        }
+        if (line.startsWith('N/A|')) {
+          console.log(line);
+        }
+      });
+
+      ParseTableHeaders(tokens);
+      buffer.write(chunkstr);
+      size += chunk.length;
+
+
+    }
+  });
+  file.on('end', () => {
+    console.log('There will be no more data.');
+  });
+}
+ParseExtract();
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
