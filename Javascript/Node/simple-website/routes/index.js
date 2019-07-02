2461bf3bc659f25f0aee20f55f2585dfc83af278
KumaranM <kumaranm2040@gmail.com>
2019-07-02 21:47:33 +0200

    Added simple address handling using an array. Page now displays address correctly

--- Javascript/Node/simple-website/routes/index.js
+++ Javascript/Node/simple-website/routes/index.js
@@ -4,6 +4,7 @@ const extractparser = require('../extract-parser');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let account = extractparser.statementObject.accounts[0].details;
+  account["combinedAddress"] = [account.AddressLine1, account.AddressLine2, account.AddressLine3, account.AddressLine4, account.AddressLine5, account.AddressLine6, account.AddressLine7, account.AddressLine8];
   res.render('index', { title: 'Express', account});
 });
 
