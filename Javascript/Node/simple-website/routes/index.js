51363e716ba14c44f36d0c4eaadd880d6f6f79fe
KumaranM <kumaranm2040@gmail.com>
2019-07-03 20:29:51 +0200

    Added Error page due to nagging from nunjucks. Moved the SAHL details from statement into its own njk file.

--- Javascript/Node/simple-website/routes/index.js
+++ Javascript/Node/simple-website/routes/index.js
@@ -6,9 +6,6 @@ router.get('/', function(req, res, next) {
   let account = extractparser.statementObject.accounts[0].details;
   account["combinedAddress"] = [account.AddressLine1, account.AddressLine2, account.AddressLine3, account.AddressLine4, account.AddressLine5, account.AddressLine6, account.AddressLine7, account.AddressLine8];
   console.log(account);
-  let c = parseFloat(account.LoanAgreementAmount);
-  let a  = account.LoanAgreementAmount.toLocaleString('en',{useGrouping: true});
-  let d = c.toLocaleString('en',{style : 'currency', currency: 'R', useGrouping: false, minimumFractionDigits: 2, maximumFractionDigits: 2});
   res.render('index', { title: 'Express', account});
 });
 
