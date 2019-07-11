756cdad0f02bacadfa968f2d0de653cc8325e273
KumaranM <kumaranm2040@gmail.com>
2019-07-11 22:36:59 +0200

    Fxied display of Loan Agreement Amount: field

--- Javascript/Node/simple-website/routes/index.js
+++ Javascript/Node/simple-website/routes/index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const extractparser = require('../extract-parser');
+
+function numberWithSpaces(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let account = extractparser.statementObject.accounts[0].details;
   account["combinedAddress"] = [account.AddressLine1, account.AddressLine2, account.AddressLine3, account.AddressLine4, account.AddressLine5, account.AddressLine6, account.AddressLine7, account.AddressLine8];
   console.log(account);
+  console.log(numberWithSpaces(account.LoanAgreementAmount));
+  account.LoanAgreementAmount = numberWithSpaces(account.LoanAgreementAmount);
   res.render('index', { title: 'Express', account});
 });
 
