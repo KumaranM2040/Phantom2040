9474486edb98fd7e237a89981c6a718f509c899f
Kumaran Moodley <kumaranm2040@gmail.com>
2020-01-25 21:44:28 +0200

    switched to HTTPS using letsencrypt certificate using certbot

--- Javascript/Node/RasberryPi-IOT/rpiIOT/gulpfile.js
+++ Javascript/Node/RasberryPi-IOT/rpiIOT/gulpfile.js
@@ -14,13 +14,43 @@ const sass = require("gulp-sass");
 const uglify = require("gulp-uglify");
 const Gpio = require("onoff").Gpio;
 
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/silverlanternslight.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/silverlanternslight.com/fullchain.pem', 'utf8');
+//const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+};
+
+
+// Starting both http & https servers
+//const httpServer = http.createServer(app);
+//const httpsServer = https.createServer(credentials, app);
+
+// httpServer.listen(80, () => {
+// 	console.log('HTTP Server running on port 80');
+// });
+
+// httpsServer.listen(443, () => {
+// 	console.log('HTTPS Server running on port 443');
+// });
+
+
+
+
 // const Gpio = class { writeSync(){ true;} 
 //                     readSync(){ true;} 
 //                     unexport(){ true;} 
 //                   }//require("onoff").Gpio;
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
+const server = https.createServer(credentials, app);
 
 app.use(express.static('./'));
 
@@ -93,7 +123,7 @@ GPIOControllerSocket.on("connection", function(socket) {
 
 });
 }
-server.listen(3000);
+server.listen(443);
 
 
 // Load package.json for banner
