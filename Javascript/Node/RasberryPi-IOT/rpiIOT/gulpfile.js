d191a751ff4bc6e55427bdbd9e91fd20a25f5b3c
Kumaran Moodley <kumaranm2040@gmail.com>
2020-01-28 21:44:41 +0200

    Reduced queries made to Cloudflare as we can cache the DNS A value in Cloudflare and only query if Public ip has changed and different from Cloudflare DNS A value

--- Javascript/Node/RasberryPi-IOT/rpiIOT/gulpfile.js
+++ Javascript/Node/RasberryPi-IOT/rpiIOT/gulpfile.js
@@ -20,6 +20,8 @@ const express = require("express");
 const app = express();
 const request = require("request-promise-native");
 var nodeServer;
+var lastPublicIpAddress = '';
+var lastDnsARecord = '';
 
 function updateCloudFlareDNSARecordIfRequired() {
   var prom = new Promise(function(resolve, reject) {
@@ -37,6 +39,12 @@ function updateCloudFlareDNSARecordIfRequired() {
 
     function onSuccess(currentPublicIP) {
       console.log("Our current Public IP is " + currentPublicIP);
+      if (currentPublicIP === lastPublicIpAddress){
+        console.log("Our IP has not changed since the last time we checked. No need to query Cloudflare"); 
+        resolve();
+         return;
+      }
+        
       const options = {
         url:
           "https://api.cloudflare.com/client/v4/zones/9ec329dd044850e58adaf4b438a55530/dns_records?type=A&name=silverlanternslight.com&status=active&account.id=77a3d8b49911c385263fb6627104a40b&page=1&per_page=20&order=status&direction=desc&match=all",
@@ -46,11 +54,20 @@ function updateCloudFlareDNSARecordIfRequired() {
           "X-Auth-Key": "e6a6ea969e14bcf8832ea36fd2d74730bf267"
         }
       };
+      if (lastDnsARecord.length > 0 && lastDnsARecord === currentPublicIP)
+      {
+         console.log("Current Ip address has changed but it already match lastDnsARecord so dont query and update Cloudflare"); 
+         resolve();
+         return;
+      }
+      
       request(options).then(
         result => {
           let dnsARecord = result.result[0].content;
           const dnsId = result.result[0].id;
           console.log(dnsARecord);
+          lastPublicIpAddress = currentPublicIP;
+          lastDnsARecord = dnsARecord;
           if (dnsARecord !== currentPublicIP && currentPublicIP.length > 0) {
             console.log(
               "Change in Public IP address detected: Current Public IP is " +
