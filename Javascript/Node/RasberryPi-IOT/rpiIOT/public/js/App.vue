263df3058b7f83b5fea2a611d3103c3c12df52e8
Kumaran Moodley <kumaranm2040@gmail.com>
2020-05-17 18:37:05 +0200

    performed npm update and npm audit fix

---
+++ Javascript/Node/RasberryPi-IOT/rpiIOT/public/js/App.vue
@@ -0,0 +1,24 @@
+  <template>
+    <b-calendar v-model="value" @context="onContext" block locale="en-US"></b-calendar>
+  </template>
+
+<style>
+  body { padding: 1rem; }
+</style>
+
+<script>
+  export default {
+      data() {
+        return {
+          value: '',
+          context: null
+        }
+      },
+      methods: {
+        onContext(ctx) {
+          console.log(ctx.selectedYMD);
+          this.context = ctx
+        }
+      }
+    }
+</script>
