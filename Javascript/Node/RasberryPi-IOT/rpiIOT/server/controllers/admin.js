ba22731679f5bf2ce03a5d86087da448676a14f0
Kumaran Moodley <kumaranm2040@gmail.com>
2020-06-05 22:22:26 +0200

    Adds Vuetify Calendar app into the admin route. Need to check how to post the calendar event data to server next

--- Javascript/Node/RasberryPi-IOT/rpiIOT/server/controllers/admin.js
+++ Javascript/Node/RasberryPi-IOT/rpiIOT/server/controllers/admin.js
@@ -2,7 +2,7 @@ const users = require('../models/users');
 
 function getAdmin(req, res, next) {
     if (req.session.IsAuthenticated) {
-        res.render('html-templates/scheduler.html', { isAdmin: req.session.IsAuthenticated, User: req.session.User });
+        res.render('html-templates/index.html', { isAdmin: req.session.IsAuthenticated, User: req.session.User });
     }
 }
 module.exports = { getAdmin };
