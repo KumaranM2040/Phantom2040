781e8f4bcaa22cdf26e108c30091737bf5809a81
Kumaran <kumaranm2040@gmail.com>
2022-11-19 21:27:32 +0200

    Updated package.json by adding prettier * Added Get Public IP Address URL as argument and propagate via yargs * Ran "npx prettier --write --single-quote ." and confirmed files have been prettified.

--- JavaScript/server-side/Node/CloudFlareDNSUpdater/CloudFlareAPI.js
+++ JavaScript/server-side/Node/CloudFlareDNSUpdater/CloudFlareAPI.js
@@ -1,77 +1,98 @@
-const axios = require("axios");
+const axios = require('axios');
 
 class CloudFlareAPI {
+   constructor(email, key, zoneId, accountId, domainName, publicIpUrl) {
+      console.log('Hit constructor');
+      this.CloudFlareDNSAIpAddress = '';
+      this.email = email;
+      this.key = key;
+      this.zoneId = zoneId;
+      this.domainName = domainName;
+      this.accountId = accountId;
+      this.publicIpUrl = publicIpUrl;
+      this.CloudFlareApiUrl = 'https://api.cloudflare.com/client/v4/zones/';
+   }
 
-    constructor(email, key, zoneId, accountId, domainName) {
-        console.log('Hit constructor');
-        this.CloudFlareDNSAIpAddress = '';
-        this.email = email;
-        this.key = key;
-        this.zoneId = zoneId;
-        this.domainName = domainName;
-        this.accountId = accountId;
-    }
+   async getHostPublicIp() {
+      try {
+         const response = await axios(this.publicIpUrl);
+         return response.data;
+      } catch (error) {
+         console.error(error);
+      }
+   }
 
-    async getHostPublicIp() {
-        try
-        {
-            const response = await axios("https://api.ipify.org");
-            return response.data;
-        } catch (error) {
-            console.error(error);
-        }
-    }
+   async updateCloudflareDNSA(currentPublicIP) {
+      console.log('Our current Public IP is ' + currentPublicIP);
+      try {
+         if (currentPublicIP === this.CloudFlareDNSAIpAddress) {
+            console.log(
+               'Our IP has not changed since the last time we checked. No need to query Cloudflare'
+            );
+            return;
+         }
+         const headers = {
+            'X-Auth-Email': this.email,
+            'X-Auth-Key': this.key,
+            'content-type': 'application/json',
+         };
+         const getCloudFlareDNSAOptions = {
+            url:
+               this.CloudFlareApiUrl +
+               this.zoneId +
+               '/dns_records?type=A&name=' +
+               this.domainName +
+               '&status=active&account.id=' +
+               this.accountId +
+               '&page=1&per_page=20&order=type&direction=desc&match=all',
+            json: 'true',
+            headers,
+         };
 
-    async updateCloudflareDNSAIpIfDifferent(currentPublicIP) {
-        console.log("Our current Public IP is " + currentPublicIP);
-        try {
-            if (currentPublicIP === this.CloudFlareDNSAIpAddress) {
-                console.log("Our IP has not changed since the last time we checked. No need to query Cloudflare");
-                return;
-            }
-            const headers = {
-                "X-Auth-Email": this.email,
-                "X-Auth-Key": this.key,
-                "content-type": "application/json"
+         const response = await axios(getCloudFlareDNSAOptions);
+         const result = response.data;
+         let dnsARecord = result.result[0].content;
+         const dnsId = result.result[0].id;
+         console.log(
+            'CloudFlare DNSA record Ip Address for ' +
+               this.domainName +
+               ' is ' +
+               dnsARecord
+         );
+         this.CloudFlareDNSAIpAddress = dnsARecord;
+         if (dnsARecord !== currentPublicIP && currentPublicIP.length > 0) {
+            console.log(
+               'Change in Public IP address detected: Current Public IP is ' +
+                  currentPublicIP +
+                  ' dnsARecord is ' +
+                  dnsARecord
+            );
+            const url =
+               this.CloudFlareApiUrl + this.zoneId + '/dns_records/' + dnsId;
+            const patchCloudFlareDNSA = {
+               method: 'PATCH',
+               url,
+               headers,
+               data: JSON.stringify({
+                  type: 'A',
+                  name: this.domainName,
+                  content: currentPublicIP,
+                  ttl: 1,
+                  proxied: false,
+               }),
             };
-            const getCloudFlareDNSAOptions = {
-                url: 'https://api.cloudflare.com/client/v4/zones/'+this.zoneId+'/dns_records?type=A&name='+this.domainName+'&status=active&account.id='+this.accountId+'&page=1&per_page=20&order=type&direction=desc&match=all',
-                json: "true",
-                headers
-            };
-
-            const response = await axios(getCloudFlareDNSAOptions);
-            const result = response.data;
-            let dnsARecord = result.result[0].content;
-            const dnsId = result.result[0].id;
-            console.log('CloudFlare DNSA record Ip Address for '+ this.domainName +' is ' + dnsARecord);
-            this.CloudFlareDNSAIpAddress = dnsARecord
-            if (dnsARecord !== currentPublicIP && currentPublicIP.length > 0) {
-                console.log("Change in Public IP address detected: Current Public IP is " + currentPublicIP + " dnsARecord is " + dnsARecord);
-                const url = 'https://api.cloudflare.com/client/v4/zones/'+ this.zoneId+ '/dns_records/'+ dnsId;
-                const patchCloudFlareDNSA = {
-                    method: 'PATCH',
-                    url,
-                    headers,
-                    data: JSON.stringify({
-                        type: "A",
-                        name: this.domainName,
-                        content: currentPublicIP,
-                        ttl: 1,
-                        proxied: false
-                    })
-                };
-                const res = await axios(patchCloudFlareDNSA);
-                console.log("Successfully update Cloudflare DNS A record for Domain silverlanternslight");
-                this.CloudFlareDNSAIpAddres = currentPublicIP;
-            } else if (currentPublicIP.length > 0) {
-                console.log("No change in Public IP address " + currentPublicIP);
-            }
-        }
-        catch (error) {
-            console.error(error);
-        }
-    }
+            const res = await axios(patchCloudFlareDNSA);
+            console.log(
+               'Successfully update Cloudflare DNS A record for Domain silverlanternslight'
+            );
+            this.CloudFlareDNSAIpAddres = currentPublicIP;
+         } else if (currentPublicIP.length > 0) {
+            console.log('No change in Public IP address ' + currentPublicIP);
+         }
+      } catch (error) {
+         console.error(error);
+      }
+   }
 }
 
 module.exports = CloudFlareAPI;
--- JavaScript/server-side/Node/CloudFlareDNSUpdater/index.js
+++ JavaScript/server-side/Node/CloudFlareDNSUpdater/index.js
@@ -3,54 +3,69 @@ const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 const CloudFlareAPI = require('./CloudFlareAPI');
 const argv = yargs(hideBin(process.argv))
-  .example('$0 ', 'Checks current IP address and compares to CloudFlare DNSA entry, If different update')
-  .demandOption(['e','k','z','d','a','f'])
-  .help('h')
-  .alias('h', 'help')
-  .epilog('copyright 2019')
-  .option('verbose', {
-    alias: 'v',
-    type: 'boolean',
-    description: 'Run with verbose logging'
-  })
-  .option('frequency', {
-    alias: 'f',
-    type: 'number',
-    description: 'number of minutes between checking/updating DNSA IP Address in CloudFlare'
-  })
-  .option('accountId', {
-    alias: 'a',
-    type: 'string',
-    description: 'Cloudflare Account Id'
-  })
-  .option('zoneId', {
-    alias: 'z',
-    type: 'string',
-    description: 'Cloudflare Zone Id'
-  })
-  .option('domainName', {
-    alias: 'd',
-    type: 'string',
-    description: 'Cloudflare managed Domain Name'
-  })
-  .option('email', {
-    alias: 'e',
-    type: 'string',
-    description: 'Email address used in the X-Auth header'
-  })
-  .option('key', {
-    alias: 'k',
-    type: 'string',
-    description: 'Key used in the X-Auth header'
-  })
-  .argv
+   .example(
+      '$0 ',
+      'Checks current IP address and compares to CloudFlare DNSA entry, If different update'
+   )
+   .demandOption(['e', 'k', 'z', 'd', 'a', 'f', 'i'])
+   .help('h')
+   .alias('h', 'help')
+   .epilog('copyright 2019')
+   .option('verbose', {
+      alias: 'v',
+      type: 'boolean',
+      description: 'Run with verbose logging',
+   })
+   .option('frequency', {
+      alias: 'f',
+      type: 'number',
+      description:
+         'number of minutes between checking/updating DNSA IP Address in CloudFlare',
+   })
+   .option('accountId', {
+      alias: 'a',
+      type: 'string',
+      description: 'Cloudflare Account Id',
+   })
+   .option('zoneId', {
+      alias: 'z',
+      type: 'string',
+      description: 'Cloudflare Zone Id',
+   })
+   .option('domainName', {
+      alias: 'd',
+      type: 'string',
+      description: 'Cloudflare managed Domain Name',
+   })
+   .option('email', {
+      alias: 'e',
+      type: 'string',
+      description: 'Email address used in the X-Auth header',
+   })
+   .option('key', {
+      alias: 'k',
+      type: 'string',
+      description: 'Key used in the X-Auth header',
+   })
+   .option('publicIpUrl', {
+      alias: 'i',
+      type: 'string',
+      description: 'URL to acquire public IP Address',
+   }).argv;
 
-const client = new CloudFlareAPI(argv.email, argv.key, argv.zoneId, argv.accountId, argv.domainName);
+const client = new CloudFlareAPI(
+   argv.email,
+   argv.key,
+   argv.zoneId,
+   argv.accountId,
+   argv.domainName,
+   argv.publicIpUrl
+);
 
-async function checkForUpdates(){
-  console.log('checkForUpdates');
-  let currentPublicIp = await client.getHostPublicIp();
-  await client.updateCloudflareDNSAIpIfDifferent(currentPublicIp);
+async function checkForUpdates() {
+   console.log('checkForUpdates');
+   let currentPublicIp = await client.getHostPublicIp();
+   await client.updateCloudflareDNSA(currentPublicIp);
 }
 checkForUpdates();
 setInterval(checkForUpdates, argv.frequency * 60 * 1000);
